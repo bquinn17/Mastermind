47fdc41664ac1413138f1e71db505f6679f4c605
blq9064 <blq9064@rit.edu>
2016-04-23 13:04:41 -0400

    Initial commit.

---
+++ src/MastermindGraphicalVC.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author Bryan Quinn
+ * file: MastermindGraphicalVC.java
+ *
+ */
+public class MastermindGraphicalVC extends Application implements Observer{
+
+    public MastermindGraphicalVC(){
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
